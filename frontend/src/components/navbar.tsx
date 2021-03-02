c56c0f7a86159010ac6a3ed03fa8fcc3e29f97f7
lucinda <lucinda.onguyen@gmail.com>
2021-03-01 21:41:01 -0600

    Add a navbar from bootstrap

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,36 +1,26 @@
 import React, { Component } from "react";
-import { Navbar, NavDropdown, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { Container, Row, Col, Navbar, NavDropdown, Nav, Form, FormControl, Button } from "react-bootstrap";
 
 class NavBar extends Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <div>
+<Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">Cultured Foodies</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/countries">Countries</Nav.Link>
+      <Nav.Link href="/recipes">Recipes</Nav.Link>
+      <Nav.Link href="/news">News</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+    </div>
     );
   }
 }
