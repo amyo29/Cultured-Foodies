22a15ce15b84cc5dc263a1f907188b932f9813a4
Vishal Tak <vishal.tak@shoplc.com>
2021-03-02 14:11:23 -0600

    Updated the readme and added logo images for the about page.

---
+++ frontend/src/pages/About/AboutInfo.tsx
@@ -0,0 +1,39 @@
+import AWSLogo from "./Images/AWSLogo.png"
+import BlackLogo from "./Images/BlackLogo.png"
+import GitLabLogo from "./Images/GitLabLogo.png"
+import PostmanLogo from "./Images/PostmanLogo.png"
+import ReactLogo from "./Images/ReactLogo.png"
+
+const toolInfo = [
+    {
+        title: "AWS",
+        img: AWSLogo,
+        description:"A JavaScript library for building user interfaces",
+        link: "https://reactjs.org/",
+    },
+    {
+        title: "Black",
+        img: BlackLogo,
+        description:"A JavaScript library for building user interfaces",
+        link: "https://reactjs.org/",
+    },
+    {
+        title: "GitLab",
+        img: GitLabLogo,
+        description:"A JavaScript library for building user interfaces",
+        link: "https://reactjs.org/",
+    },
+    {
+        title: "React",
+        img: ReactLogo,
+        description:"A JavaScript library for building user interfaces",
+        link: "https://reactjs.org/",
+    },
+    {
+        title: "React",
+        img: ReactLogo,
+        description:"A JavaScript library for building user interfaces",
+        link: "https://reactjs.org/",
+    },
+
+]
