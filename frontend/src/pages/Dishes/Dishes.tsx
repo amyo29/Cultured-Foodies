7e41d92e19383bc6245b51889c0edb2c7378e8a3
lucinda <lucinda.onguyen@gmail.com>
2021-03-02 20:53:38 -0600

    add grid for dishes, closes #14

--- frontend/src/pages/Dishes/Dishes.tsx
+++ frontend/src/pages/Dishes/Dishes.tsx
@@ -1,31 +1,32 @@
 import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
+import Footer from "../../components/Footer";
 
-const afghan_dish = require("../../data/AfghanistanDishes.json");
-const albania_dish = require("../../data/AlbaniaDishes.json");
-const algeria_dish = require("../../data/AlgeriaDishes.json");
+const afghan_dish = require("../../data/AfghanistanDishes.json")["hits"].slice(0,3);
+const albania_dish = require("../../data/AlbaniaDishes.json")["hits"].slice(0, 3);
+const algeria_dish = require("../../data/AlgeriaDishes.json")["hits"].slice(0,3);
 
 function Dishes() {
-  let rows = [
-    [{ country: "United States", desc: "hello, Country" }, { fake: 2 }],
-    [{ fake: 3 }, { fake: 4 }],
-  ];
-  //
+  var rows = [[afghan_dish], [albania_dish], [algeria_dish]].reduce(
+    (accum, el) => accum.concat(el),
+    []
+  );
   return (
     <div>
       <header>Countries</header>
       <Container>
         {rows.map((cols) => (
           <Row>
-            {cols.map((col) => (
+            {cols.map((col: any) => (
               <Col>
+              <a href= {"/dishes/"+ col["recipe"]["label"]}>
                 <Card>
                   {/* <Card.Img variant="top" src={img} /> */}
                   <Card.Body>
-                    <Card.Title>{col["country"]}</Card.Title>
-                    <Card.Text>{col["desc"]}</Card.Text>
+                    <Card.Text>{col["recipe"]["label"]}</Card.Text>
                   </Card.Body>
                 </Card>
+                </a>
               </Col>
             ))}
           </Row>
