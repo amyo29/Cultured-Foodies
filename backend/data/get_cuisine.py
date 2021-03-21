d3abbae4fcf58022c3dd7c9367e2792335e3d51e
Tim Nguyen <nguye@MacBook-Pro-2.local>
2021-03-20 21:08:26 -0500

    Added countryData to each cuisine in all_cuisines.json

--- backend/data/get_cuisine.py
+++ backend/data/get_cuisine.py
@@ -4,117 +4,119 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-cuisine_country_dict = {"Thai": "Thailand",
-                        "Bangladeshi": "Bangladesh",
-                        "Tex-Mex": "United States of America",
-                        "Cuban": "Cuba",
-                        "German": "Germany",
-                        "Paraguayan": "Paraguay",
-                        "Taiwanese": "Taiwan",
-                        "Nicaraguan": "Nicaragua",
-                        "Trinidad And Tobago": "Trinidad And Tobago",
-                        "Greek": "Greece",
-                        "Scandinavian": "Norway",
-                        "Southwestern": "United States of America",
-                        "Moroccan": "Morocco",
-                        "Creole": "United States of America",
-                        "Albanian": "Albania",
-                        "British": "United Kingdom of Great Britain and Northern Ireland",
-                        "Australian": "Australia",
-                        "Iranian": "Iran",
-                        "Welsh": "United Kingdom of Great Britain and Northern Ireland",
-                        "Israeli": "Israel",
-                        "Mongolian": "Mongolia",
-                        "Amish": "United States of America",
-                        "Canadian": "Canada",
-                        "American": "United States of America",
-                        "Singaporean": "Singapore",
-                        "Ukrainian": "Ukraine",
-                        "Spanish": "Spain",
-                        "Irish": "Ireland",
-                        "Vietnamese": "Vietnam",
-                        "New American": "United States of America",
-                        "Armenian": "Armenia",
-                        "Colombian": "Colombia",
-                        "Moldovan": "Moldova",
-                        "New Mexican": "United States of Ammerica",
-                        "Syrian": "Syria",
-                        "Pakistani": "Pakistan",
-                        "Hungarian": "Hungary",
-                        "Guyanese": "Guyana",
-                        "Bolivian": "Bolivia",
-                        "California": "United States of America",
-                        "Turkish": "Turkey",
-                        "Yemeni": "Yemen",
-                        "Tibetan": "China",
-                        "Chinese": "China",
-                        "Jewish": "Israel",
-                        "Korean": "Korea",
-                        "Nepalese": "Nepal",
-                        "Cambodian": "Cammbodia",
-                        "Sri Lankan": "Sri Lanka",
-                        "Southern": "United States of America",
-                        "Caribbean": "Belize",
-                        "Mexican": "Mexico",
-                        "Peruvian": "Peru",
-                        "Afghan": "Afghanistan",
-                        "Dominican": "Dominican Republic",
-                        "Austrian": "Austria",
-                        "New Zealand": "New Zealand",
-                        "Danish": "Denmark",
-                        "Icelandic": "Iceland",
-                        "Punjabi": "India",
-                        "Filipino": "Philippines",
-                        "Chilean": "Chile",
-                        "Cajun": "United States of America",
-                        "Balearic": "Spain",
-                        "Portuguese": "Portugal",
-                        "Somali": "Somalia",
-                        "Ecuadorian": "Ecuador",
-                        "Burmese": "Myanmar",
-                        "Soul Food": "United States of America",
-                        "Ethiopian": "Ethiopia",
-                        "Saudi Arabian": "Saudi Arabia",
-                        "Hong Kong": "China",
-                        "Sichuan": "China",
-                        "Tunisian": "Tunisia",
-                        "Nigerian": "Nigeria",
-                        "Jamaican": "Jamaica",
-                        "Brazilian": "Brazil",
-                        "French": "France",
-                        "Salvadorean": "El Salvador",
-                        "Venezuelan": "Venezuela",
-                        "Uruguayan": "Uruguay",
-                        "Indonesian": "Indonesia",
-                        "Cantonese": "China",
-                        "South African": "South Africa",
-                        "Uzbek": "Uzbekistan",
-                        "Puerto Rican": "Puerto Rico",
-                        "Swedish": "Sweden",
-                        "Haitian": "Haiti",
-                        "Polish": "Poland",
-                        "Scottish": "Scotland",
-                        "Italian": "Italy",
-                        "Russian": "Russia",
-                        "Georgian": "Georgia",
-                        "Japanese": "Japan",
-                        "Indian": "India",
-                        "Argentine": "Argentina",
-                        "Swiss": "Sweden",
-                        "Hawaiian": "United States of America",
-                        "Lebanese": "Lebanon",
-                        "Malaysian": "Malaysia",
-                        "Belgian": "Belgium"
-                        }
+cuisine_country_dict = {
+    "Thai": "Thailand",
+    "Bangladeshi": "Bangladesh",
+    "Tex-Mex": "United States of America",
+    "Cuban": "Cuba",
+    "German": "Germany",
+    "Paraguayan": "Paraguay",
+    "Taiwanese": "Taiwan",
+    "Nicaraguan": "Nicaragua",
+    "Trinidad And Tobago": "Trinidad And Tobago",
+    "Greek": "Greece",
+    "Scandinavian": "Norway",
+    "Southwestern": "United States of America",
+    "Moroccan": "Morocco",
+    "Creole": "United States of America",
+    "Albanian": "Albania",
+    "British": "United Kingdom of Great Britain and Northern Ireland",
+    "Australian": "Australia",
+    "Iranian": "Iran",
+    "Welsh": "United Kingdom of Great Britain and Northern Ireland",
+    "Israeli": "Israel",
+    "Mongolian": "Mongolia",
+    "Amish": "United States of America",
+    "Canadian": "Canada",
+    "American": "United States of America",
+    "Singaporean": "Singapore",
+    "Ukrainian": "Ukraine",
+    "Spanish": "Spain",
+    "Irish": "Ireland",
+    "Vietnamese": "Vietnam",
+    "New American": "United States of America",
+    "Armenian": "Armenia",
+    "Colombian": "Colombia",
+    "Moldovan": "Moldova",
+    "New Mexican": "United States of Ammerica",
+    "Syrian": "Syria",
+    "Pakistani": "Pakistan",
+    "Hungarian": "Hungary",
+    "Guyanese": "Guyana",
+    "Bolivian": "Bolivia",
+    "California": "United States of America",
+    "Turkish": "Turkey",
+    "Yemeni": "Yemen",
+    "Tibetan": "China",
+    "Chinese": "China",
+    "Jewish": "Israel",
+    "Korean": "Korea",
+    "Nepalese": "Nepal",
+    "Cambodian": "Cammbodia",
+    "Sri Lankan": "Sri Lanka",
+    "Southern": "United States of America",
+    "Caribbean": "Belize",
+    "Mexican": "Mexico",
+    "Peruvian": "Peru",
+    "Afghan": "Afghanistan",
+    "Dominican": "Dominican Republic",
+    "Austrian": "Austria",
+    "New Zealand": "New Zealand",
+    "Danish": "Denmark",
+    "Icelandic": "Iceland",
+    "Punjabi": "India",
+    "Filipino": "Philippines",
+    "Chilean": "Chile",
+    "Cajun": "United States of America",
+    "Balearic": "Spain",
+    "Portuguese": "Portugal",
+    "Somali": "Somalia",
+    "Ecuadorian": "Ecuador",
+    "Burmese": "Myanmar",
+    "Soul Food": "United States of America",
+    "Ethiopian": "Ethiopia",
+    "Saudi Arabian": "Saudi Arabia",
+    "Hong Kong": "China",
+    "Sichuan": "China",
+    "Tunisian": "Tunisia",
+    "Nigerian": "Nigeria",
+    "Jamaican": "Jamaica",
+    "Brazilian": "Brazil",
+    "French": "France",
+    "Salvadorean": "El Salvador",
+    "Venezuelan": "Venezuela",
+    "Uruguayan": "Uruguay",
+    "Indonesian": "Indonesia",
+    "Cantonese": "China",
+    "South African": "South Africa",
+    "Uzbek": "Uzbekistan",
+    "Puerto Rican": "Puerto Rico",
+    "Swedish": "Sweden",
+    "Haitian": "Haiti",
+    "Polish": "Poland",
+    "Scottish": "Scotland",
+    "Italian": "Italy",
+    "Russian": "Russia",
+    "Georgian": "Georgia",
+    "Japanese": "Japan",
+    "Indian": "India",
+    "Argentine": "Argentina",
+    "Swiss": "Sweden",
+    "Hawaiian": "United States of America",
+    "Lebanese": "Lebanon",
+    "Malaysian": "Malaysia",
+    "Belgian": "Belgium",
+}
 
-
-options = Options()
-options.headless = True
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 def get_world_food_per_cuisine(cuisine):
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     cuisine = cuisine.replace(" ", "_")
-    driver.get("https://worldfood.guide/cuisinedishes/" + cuisine.lower() + "/" + str(1) + "/")
+    driver.get(
+        "https://worldfood.guide/cuisinedishes/" + cuisine.lower() + "/" + str(1) + "/"
+    )
     elements = driver.find_elements_by_class_name("title3.color2")
     result = []
     for item in elements:
@@ -122,12 +124,13 @@ def get_world_food_per_cuisine(cuisine):
 
     return result
 
+
 def get_description(cuisine):
     # import pdb
     # pdb.set_trace
-    # options = Options()
-    # options.headless = True
-    # driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     cuisine = cuisine.replace(" ", "_")
     url = "https://worldfood.guide/cuisine/" + cuisine.lower() + "/"
     print("url: ", url)
@@ -137,6 +140,7 @@ def get_description(cuisine):
     for item in elements:
         description += item.text
     return description
+
 
 def get_world_food_dishes():
     result = []
@@ -153,10 +157,31 @@ def get_world_food_dishes():
         cuisine_dict["description"] = get_description(cuisine)
         result.append(cuisine_dict)
 
-    f = open("all_cuisines.json", 'w')
+    f = open("all_cuisines.json", "w")
     json.dump(result, f)
     print(not_found)
 
-get_world_food_dishes() 
 
-         
+def find_country(country):
+    countries_f = open("countryData.json")
+    countries_data = json.load(countries_f)
+
+    for country_data in countries_data:
+        if country in country_data["name"]:
+            return country_data
+    return "no country or multiple countries"
+
+
+def get_country_data():
+    cuisines_f = open("all_cuisines.json")
+    cuisines_data = json.load(cuisines_f)
+
+    for i in range(0, len(cuisines_data)):
+        cuisine = cuisines_data[i]
+        cuisine["countryData"] = find_country(cuisine["country"])
+
+    with open("all_cuisines.json", "w") as file:
+        json.dump(cuisines_data, file)
+
+
+get_country_data()
