1ec35640299097be2b5f6937587a557e877f3bae
Gear-500 <joshuanms@gmail.com>
2021-03-23 17:53:21 -0500

    Create About.test.tsx
    Doesn't work though

---
+++ frontend/src/pages/About/About.test.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import { expect } from "chai";
+
+import About from "./About";
+import { API_INFO, TOOLS_INFO, TEAM_INFO } from "./AboutInfo";
+
+/*
+TODO: Things to test (once each): 
+1) The whole About page
+2) team cards  
+3) Stats
+ */
+
+let rootContainer = null; 
+
+beforeEach(() => {
+    rootContainer = document.createElement("div");
+    document.body.appendChild(rootContainer);
+});
+
+afterEach(() => {
+    document.body.removeChild(rootContainer);
+    rootContainer = null;
+});
+
+describe("", () => {
+    it("Renders the About page", () => {
+        // ensure that component is rendered and mounted
+        act(() => {
+            ReactDOM.render(<About />, rootContainer);
+        }) 
+        
+    });
+});
