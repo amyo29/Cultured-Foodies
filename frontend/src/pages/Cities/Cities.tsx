5906d0680bcf3c67f855ed16452facdad3d17f3c
Siddhesh Krishnan <siddheshkrishnan1@gmail.com>
2021-03-24 20:17:44 -0500

    Added pagination for cities, Fixes #48

--- frontend/src/pages/Cities/Cities.tsx
+++ frontend/src/pages/Cities/Cities.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import { Container, Row, Col, Card, ListGroup, Navbar } from "react-bootstrap";
 import Footer from "../../components/Footer";
 import useAxios from 'axios-hooks'
+import { Pagination } from '@material-ui/lab';
 // const data = require("../../data/threeDishes.json");
 const data1 = require("../../data/newData/threeCities.json");
 
@@ -10,6 +11,10 @@ function Cities() {
     document.title = "Cities"
   }, [])
   const [cities, setCities] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const handleChange = (event:any, value:number) => {
+    setPageNumber(value);
+  };
   const [{ data, loading, error }] = useAxios('/api/cities')
   useEffect(() => {
     document.title = "Cities"
@@ -23,39 +28,55 @@ function Cities() {
   },[data])
 
 
-
+  const numPerPage = 12;
+  const startIndex= numPerPage*pageNumber;
+  const currentData = cities.slice(startIndex, startIndex+numPerPage);
+  console.log("Cities", cities);
   var i, j;
-  var chunk = 5;
+  var chunk = 3;
   var rows = [];
-  for (i = 0, j = data1.length; i < j; i += chunk) {
-    rows.push(data1.slice(i, i + chunk));
+  for (i = 0, j = currentData.length; i < j; i += chunk) {
+    rows.push(currentData.slice(i, i + chunk));
   }
+
   return (
     <div>
       <h1 className="text-align center">Cities</h1>
       <Container>
+        {/* {currentData.map((city) => (
+          <div>
+            {city["name"]}
+          </div>
+        ))} */}
         {rows.map((cols) => (
           <Row>
-            {cols.map((col: any, i: any) => (
+            {cols.map((city: any, i: any) => (
               <Col className = "col-sm-4 py-2">
+         
+                
                 <Card bg='card h-100'>
                   <Card.Body>
-                    <a href= {"/cities/"+ i}><Card.Title>{col["name"]}</Card.Title></a>
-                    <Card.Img variant="top" src={col["images"]["mobile"]} />
+                    <a href= {"/cities/"+ i}><Card.Title>{city["name"]}</Card.Title></a>
+                    {/* <Card.Img variant="top" src={city["images"]["mobile"]} />*/}
                       <p>
-                        <b>State: </b> {col["state"]} <br />
-                        <b>Population: </b> {col["population"]} <br />
-                        <b>Latitude: </b> {col["latitude"]} <br />
-                        <b>Longitude: </b> {col["longitude"]} <br />
-                        <b>Timezone: </b> {col["timezone"]} <br />
-                      </p>
+                        <b>State: </b> {city["state"]} <br />
+                        <b>Leisure and Culture: </b> {city["leisure_culture"]} <br />
+                        <b>Cost of Living: </b> {city["cost_of_living"]} <br />
+                        <b>Environmental Quality: </b> {city["environmental_quality"]} <br />
+                        <b>Travel Connectivity: </b> {city["travel_connectivity"]} <br />
+                        <b>Population: </b> {city["population"]} <br />
+                      </p> 
                   </Card.Body>
-                </Card>
+                </Card> 
+                
+                
               </Col>
             ))}
           </Row>
         ))}
+
       </Container>
+      <Pagination count = {parseInt((cities.length/numPerPage)+"")} page={pageNumber} onChange={handleChange}></Pagination>
       {/* <Footer></Footer> */}
     </div>
   );
