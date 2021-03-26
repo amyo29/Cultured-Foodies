e2de85a1a92b2d980cf5697a5abfdd309d0e6cad
amyo29 <yawen1999@gmail.com>
2021-03-25 21:21:07 -0500

    Closes #53

--- frontend/src/pages/Restaurants/Restaurant.tsx
+++ frontend/src/pages/Restaurants/Restaurant.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect , useState} from "react";
+import React, { useEffect , useState, Component} from "react";
 import { useParams } from "react-router-dom";
 import "../../styles/Restaurant.css";
+import Card from "react-bootstrap/Card";
 import useAxios from 'axios-hooks';
 import axios from 'axios';
 import {CuisineInstance} from '../Cuisines/Cuisine';
@@ -30,7 +31,6 @@ function Restaurant() {
   }, [restaurant])
   
   
-
 
   // // model navigation
   // let countryIndex = article["countryIndex"];
@@ -68,20 +68,60 @@ function Restaurant() {
   // let source_desc = article["meta_business_summary"];
   // let topic = article["topic"];
 
+
   return (
-    <div className="center">
-      {/* <img src={img} className="article-img"></img> */}
-      <h1>{restaurant?.name}</h1>
+    // <div className="center">
+    //   {/* <img src={img} className="article-img"></img> */}
+    //   <h1>{restaurant?.name}</h1>
 
-      <section>
-    <h5>City Locations</h5>
-     <a href={"/cities/" + restaurant?.city_id}>{restaurant?.city}<br/></a>
-    <h5>Cuisines of {restaurant?.name}</h5>
-    {cuisines?.map((c) => (<a href={"/cuisines/" + c.id}>{c.name}<br/></a>))}
-    </section>
+    //   <section>
+    // <h5>City Locations</h5>
+    //  <a href={"/cities/" + restaurant?.city_id}>{restaurant?.city}<br/></a>
+    // <h5>Cuisines of {restaurant?.name}</h5>
+    // {cuisines?.map((c) => (<a href={"/cuisines/" + c.id}>{c.name}<br/></a>))}
+    // </section>
+    // </div>
+
+    <div>
+        <body className="Instance center-div">
+        <h2 className="Instance-header">{restaurant?.name}</h2>
+        <div className="info" text-align= "center">
+            <img className="instance-photo row" src={restaurant?.restaurant_image} alt=""/>
+            <Card className="instance-text row" style={{width: '34rem'}}>
+                <Card.Body>
+                    <Card.Title>{restaurant?.name}</Card.Title>
+                    <Card.Text>
+                        Address: {restaurant?.address}
+                    </Card.Text>
+                    <Card.Text>
+                        Average Rating: ${restaurant?.aggregate_rating}
+                    </Card.Text>
+                    <Card.Text>
+                        Highlights: {restaurant?.highlights}
+                    </Card.Text>
+                    <Card.Text>
+                        Price Range: {restaurant?.price_range}
+                    </Card.Text>
+                    <Card.Text>
+                        Timings: {restaurant?.timings}
+                    </Card.Text>
+                    <Card.Text>
+                        Menu: 
+                        { <a href= {restaurant?.menu_url} >{restaurant?.menu_url}<br/></a>} 
+                    </Card.Text>  
+                    <Card.Text>
+                        Cities which have this restaurant:    
+                        { <a href={"/cities/" + restaurant?.city_id}>{restaurant?.city}<br/></a>}
+                    </Card.Text>
+                    <Card.Text>
+                        Cuisines of {restaurant?.name}: 
+                        { cuisines?.map((c) => (<a href={"/cuisines/" + c.id}>{c.name}<br/></a>))}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+        </body>
     </div>
-
-
 
     //   <div className="center">
     //   <img src={img} className="article-img"></img>
