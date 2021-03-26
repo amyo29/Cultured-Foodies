61e3f29a190727f1d72c640eae5a3ef8c0b6b9df
Tim Nguyen <nguye@MacBook-Pro-2.local>
2021-03-26 01:32:49 -0500

    Fixed loading issue to make sure countries is initialized before rendering on the screen and added a dish image

--- frontend/src/pages/Cuisines/Cuisines.tsx
+++ frontend/src/pages/Cuisines/Cuisines.tsx
@@ -1,117 +1,123 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col, Card, Navbar } from "react-bootstrap";
-import useAxios from 'axios-hooks';
-import { Pagination } from '@material-ui/lab';
-import {CountryInstance, CuisineInstance} from './Cuisine'
-import axios from 'axios'
+import useAxios from "axios-hooks";
+import { Pagination } from "@material-ui/lab";
+import { CountryInstance, CuisineInstance } from "./Cuisine";
+import axios from "axios";
 function Countries() {
   useEffect(() => {
-    document.title = "Cuisines"
-  }, [])
+    document.title = "Cuisines";
+  }, []);
   const [cuisines, setCuisines] = useState([]);
   const [pageNumber, setPageNumber] = useState(1);
-  const [countries, setCountries]:any = useState({})
-  const handleChange = (event:any, value:number) => {
-    setPageNumber(value );
+  const [countries, setCountries]: any = useState({});
+  const [loaded, changeLoading] = useState(false);
+  const handleChange = (event: any, value: number) => {
+    setPageNumber(value);
   };
-  const [{ data, loading, error }] = useAxios('/api/cuisines')
+  const [{ data, loading, error }] = useAxios("/api/cuisines");
 
   useEffect(() => {
     if (data) {
-      setCuisines(data.cuisines)
-      console.log(data.cuisines)
+      setCuisines(data.cuisines);
+      // console.log(data.cuisines);
     }
 
     axios.get("/api/countries").then((value) => {
-      let countries = value["data"]["countries"]
-      console.log('countries', countries)
+      let countries = value["data"]["countries"];
+      // console.log("countries", countries);
       // let filtered = countries.filter(country:any => country['id'] ==1)
       // console.log('country', filtered)
     });
-    
-  },[data])
-/// {"id": countryObject}
+  }, [data]);
+  /// {"id": countryObject}
 
   useEffect(() => {
     axios.get("/api/countries").then((value) => {
-
-      let all_countries = value["data"]["countries"]
-      let dict_countries:any = {};
-      all_countries.forEach(function(country: any) {
-            let id: number = country["id"]
-            dict_countries[id] = country
+      let all_countries = value["data"]["countries"];
+      let dict_countries: any = {};
+      all_countries.forEach(function (country: any) {
+        let id: number = country["id"];
+        dict_countries[id] = country;
       });
-      console.log("here", dict_countries)
-      setCountries(dict_countries)
-      cuisines.forEach(function(cuisine: any) {
-        
-        let id: string = cuisine["countryID"]
-        let countries_string = id.split(", ")
-        console.log('countries Id', countries_string)
-        countries_string.forEach(function(cID: string){
-          console.log('here123', dict_countries[parseInt(cID)])
-        } );
-  });
-
+      // console.log("here", dict_countries);
+      setCountries(dict_countries);
+      changeLoading(true);
+      // cuisines.forEach(function (cuisine: any) {
+      //   let id: string = cuisine["countryID"];
+      //   let countries_string = id.split(", ");
+      //   console.log("countries Id", countries_string);
+      //   countries_string.forEach(function (cID: string) {
+      //     console.log("here123", dict_countries[parseInt(cID)]);
+      //   });
+      // });
     });
-  }, [cuisines])
-
-
+  }, [cuisines]);
 
   const numPerPage = 12;
-  const startIndex= numPerPage* (pageNumber-1);
-  const currentData = cuisines.slice(startIndex, startIndex+numPerPage);
+  const startIndex = numPerPage * (pageNumber - 1);
+  const currentData = cuisines.slice(startIndex, startIndex + numPerPage);
   var i, j;
   var chunk = 3;
   var rows = [];
   for (i = 0, j = currentData.length; i < j; i += chunk) {
     rows.push(currentData.slice(i, i + chunk));
   }
-  
+
   return (
     <div>
       <h1 className="text-align center">Cuisines</h1>
       <Container>
         {rows.map((cols) => (
           <Row>
-            {cols.map((cuisine:any, i :any) => (
+            {cols.map((cuisine: any, i: any) => (
               <Col className="col-sm-4 py-2">
-                <Card bg='card light h-100'>
+                <Card bg="card light h-100">
                   <Card.Body>
-                      <a href={"/cuisines/" + cuisine['id']}>
-                        <Card.Title>{cuisine['name']}</Card.Title>
-                      </a>
+                    <img src={cuisine.dishes[0].image_url} width="200" height="200"></img>
+                    <a href={"/cuisines/" + cuisine["id"]}>
+                      <Card.Title>{cuisine["name"]}</Card.Title>
+                    </a>
                     <Card.Text>
                       <p>
-                        <b>Country: </b>{cuisine['country']} <br />
-                        {cuisine?.countryID.split(', ').map((cID: string) =>(
-                            // <p>{ cID} </p>
-// <p>{countries[cID]?.name} </p>
+                        <b>Country: </b>
+                        {cuisine["country"]} <br />
+                        {cuisine?.countryID.split(", ").map((cID: string) => (
+                          // <p>{ cID} </p>
+                          // <p>{countries[cID]?.name} </p>
                           <>
-
-                          <p><b>Capital: </b>{countries[parseInt(cID)].capital} <br/>
-                          <b>Region: </b>{countries[parseInt(cID)].region} <br/>
-                          <b>Population:</b>{countries[parseInt(cID)].population}<br/>
-                          <b>Timezones:</b>{countries[parseInt(cID)].timezones}
-
-                           </p>
-
+                            {loaded ? (
+                              <p>
+                                <b>Capital: </b>
+                                {countries[parseInt(cID)].capital} <br />
+                                <b>Region: </b>
+                                {countries[parseInt(cID)].region} <br />
+                                <b>Population:</b>
+                                {countries[parseInt(cID)].population}
+                                <br />
+                                <b>Timezones:</b>
+                                {countries[parseInt(cID)].timezones}
+                              </p>
+                            ) : (
+                              <></>
+                            )}
                           </>
                         ))}
                       </p>
                     </Card.Text>
-                    <Card.Text>
-  
-                    </Card.Text>
+                    <Card.Text></Card.Text>
                   </Card.Body>
                 </Card>
-
               </Col>
             ))}
           </Row>
         ))}
       </Container>
-      <Pagination count = {parseInt((cuisines.length/numPerPage)+"")} page={pageNumber} onChange={handleChange}></Pagination>
+      <Pagination
+        count={parseInt(cuisines.length / numPerPage + "")}
+        page={pageNumber}
+        onChange={handleChange}
+      ></Pagination>
 
       {/* <Footer></Footer> */}
     </div>
