c1f16b56c9873030f5d1e70a9b5e09a93573f764
Gear-500 <joshuanms@gmail.com>
2021-03-27 17:13:48 -0500

    updated jest tests
    instance test not working- need to fix

--- frontend/src/tests/tests.test.tsx
+++ frontend/src/tests/tests.test.tsx
@@ -6,16 +6,55 @@ import Adapter from 'enzyme-adapter-react-16';
 import { mount, shallow } from 'enzyme';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import Home from "../pages/Home/Home";
 import About from "../pages/About/About";
+import Restaurants from "../pages/Restaurants/Restaurants";
+import Restaurant from "../pages/Restaurants/Restaurant";
+import Cities from "../pages/Cities/Cities";
+import City from "../pages/Cities/City";
+import Cuisines from "../pages/Cuisines/Cuisines";
+import Cuisine from "../pages/Cuisines/Cuisine";
+
 
 Enzyme.configure({
     adapter: new Adapter(),
 })
 
-test('About', () => {
-	const component = shallow(<About />);
-	expect(component).toMatchSnapshot();
-});
+describe("Render views", () => {
+	test('Splash Page', () => {
+		const component = shallow(<Home />);
+		expect(component).toMatchSnapshot();
+	});
+	
+	test('About', () => {
+		const component = shallow(<About />);
+		expect(component).toMatchSnapshot();
+	});
+	test('Restaurants', () => {
+		const component = shallow(<Restaurants />);
+		expect(component).toMatchSnapshot();
+	});
+	test('Cities', () => {
+		const component = shallow(<Cities />);
+		expect(component).toMatchSnapshot();
+	});
+	test('Cuisines', () => {
+		const component = shallow(<Cuisines />);
+		expect(component).toMatchSnapshot();
+	});
+	// test('Restaurant', () => {
+	// 	const component = shallow(<Restaurant/>);
+	// 	expect(component).toMatchSnapshot();
+	// });
+	// test('City', () => {
+	// 	const component = shallow(<City/>);
+	// 	expect(component).toMatchSnapshot();
+	// });
+	// test('Cuisine', () => {
+	// 	const component = shallow(<Cuisine/>);
+	// 	expect(component).toMatchSnapshot();
+	// });
+})
 
 /* about page test 
 it('Get About Page', async () => {
