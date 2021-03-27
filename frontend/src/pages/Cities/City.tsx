92ec156b3818456604a9e491ff59d7119ef533f2
amyo29 <yawen1999@gmail.com>
2021-03-27 17:33:44 -0500

    fix typos

--- frontend/src/pages/Cities/City.tsx
+++ frontend/src/pages/Cities/City.tsx
@@ -136,7 +136,7 @@ function matchesCityRestaurants(element:any, index: number, array: any){
 
       </section>
       <section>
-      <h5>Restauarants in {city?.name}</h5>
+      <h5>Restaurants in {city?.name}</h5>
       {restaurants?.map((r) => (<a href={"/restaurants/" + r.id}>{r.name}<br/></a>))}
       <h5>Cuisines {city?.name}</h5>
       {cuisines?.map((c) => (<a href={"/cuisines/" + c.id}>{c.name}<br/></a>))}
