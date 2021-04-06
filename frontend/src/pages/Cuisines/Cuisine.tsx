db9972063255b2fcf056db3039cda6d98ef0e066
Vishal Tak <vishal.tak@shoplc.com>
2021-04-05 19:45:28 -0500

    Cuisnes instances have responsive maps

--- frontend/src/pages/Cuisines/Cuisine.tsx
+++ frontend/src/pages/Cuisines/Cuisine.tsx
@@ -1,5 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Container, Col, Row, Figure, Card, CardColumns, CardDeck, CardImg } from "react-bootstrap";
+import {
+  Container,
+  Col,
+  Row,
+  Figure,
+  Card,
+  CardColumns,
+  CardDeck,
+  CardImg,
+} from "react-bootstrap";
 import Image from "react-bootstrap/Image";
 import { useParams } from "react-router-dom";
 import "../../styles/Cuisine.css";
@@ -45,79 +54,91 @@ function Cuisine(id: any) {
   console.log(countries);
 
   let API_KEY = "AIzaSyBnpJl9h_gz0umc1sVng27AS3rNZOg7LR8";
-  
+
   return (
     <div>
       <h1 className="Instance-header">{cuisine?.name}</h1>
       {countries?.map((country) => (
         <>
-        <Card> 
-          <Card.Title><h2 className="Instance-header">Country: {country?.name} </h2></Card.Title>
-      <body>
-        <div >
-          {/* <div className="row"> */}
-          
-          <CardDeck>
-            <Card border="dark">
-                <Card.Header as="h3">Basic Info:</Card.Header>
-                
-                <Card.Body>
-                  <Card.Text>Capital city: {country?.capital}</Card.Text>
-                  <Card.Text>Alpha 3 Code: {country?.alpha3code}</Card.Text>
-                  <Card.Text>Region: {country?.region}</Card.Text>
-                  <Card.Text>Subregion: {country?.subregion}</Card.Text>
-                  <Card.Text>Latitude/Longitude: {country?.latitude}, {country?.longitude}</Card.Text>
-                  <Card.Text>Area: {country?.area.toLocaleString()} km<sup>2</sup></Card.Text>
-                  <Card.Text>Population: {country?.population.toLocaleString()}</Card.Text>
-                  <Card.Text>Bordering Countries: 
-                    {country?.borders.split(", ").map((border: any) => {
-                      return <li>{border}</li>;
-                    })}
-                  </Card.Text>
-                  <Card.Text>Time Zones: {country?.timezones}</Card.Text>
-                  <Card.Text>Translations: 
-                  {country ? (
-                      Object.keys(country?.translations).map((key, index) => (
-                        <p key={index}>
-                          {" "}
-                          {key} : {country?.translations[key]}
-                        </p>
-                      ))
-                      ) 
-                      : 
-                      (
-                      <p>he</p>
-                    )}
-                  </Card.Text>
-                  <Card.Text>Dishes</Card.Text>
-                  <Card.Text>{dishes ? (
-                      dishes.map((cols: any) => (
-                        <Row>
-                          {cols.map((dish: any, i: any) => (
-                            <Col className="col-sm-4 py-2">
-                              <Figure>
-                                <Figure.Image
-                                  width={170}
-                                  height={180}
-                                  alt="171x180"
-                                  src={dish.image_url}
-                                />
-                                <Figure.Caption>{dish.name}</Figure.Caption>
-                              </Figure>
-                            </Col>
-                          ))}
-                        </Row>
-                      ))
-                    ) : (
-                      <p>loading</p>
-                    )}
-                  </Card.Text>
-              </Card.Body>
-            </Card>
-            <Card border="dark">
-              <Card.Header as="h3">Flag:</Card.Header>
-              <Card.Img src={country?.flag}></Card.Img>
-              {/* <Card.Body> 
+          <Card>
+            <Card.Title>
+              <h2 className="Instance-header">Country: {country?.name} </h2>
+            </Card.Title>
+            <body>
+              <div>
+                {/* <div className="row"> */}
+
+                <CardDeck>
+                  <Card border="dark">
+                    <Card.Header as="h3">Basic Info:</Card.Header>
+
+                    <Card.Body>
+                      <Card.Text>Capital city: {country?.capital}</Card.Text>
+                      <Card.Text>Alpha 3 Code: {country?.alpha3code}</Card.Text>
+                      <Card.Text>Region: {country?.region}</Card.Text>
+                      <Card.Text>Subregion: {country?.subregion}</Card.Text>
+                      <Card.Text>
+                        Latitude/Longitude: {country?.latitude},{" "}
+                        {country?.longitude}
+                      </Card.Text>
+                      <Card.Text>
+                        Area: {country?.area.toLocaleString()} km<sup>2</sup>
+                      </Card.Text>
+                      <Card.Text>
+                        Population: {country?.population.toLocaleString()}
+                      </Card.Text>
+                      <Card.Text>
+                        Bordering Countries:
+                        {country?.borders.split(", ").map((border: any) => {
+                          return <li>{border}</li>;
+                        })}
+                      </Card.Text>
+                      <Card.Text>Time Zones: {country?.timezones}</Card.Text>
+                      <Card.Text>
+                        Translations:
+                        {country ? (
+                          Object.keys(country?.translations).map(
+                            (key, index) => (
+                              <p key={index}>
+                                {" "}
+                                {key} : {country?.translations[key]}
+                              </p>
+                            )
+                          )
+                        ) : (
+                          <p>he</p>
+                        )}
+                      </Card.Text>
+                      <Card.Text>Dishes</Card.Text>
+                      <Card.Text>
+                        {dishes ? (
+                          dishes.map((cols: any) => (
+                            <Row>
+                              {cols.map((dish: any, i: any) => (
+                                <Col className="col-sm-4 py-2">
+                                  <Figure>
+                                    <Figure.Image
+                                      width={170}
+                                      height={180}
+                                      alt="171x180"
+                                      src={dish.image_url}
+                                    />
+                                    <Figure.Caption>{dish.name}</Figure.Caption>
+                                  </Figure>
+                                </Col>
+                              ))}
+                            </Row>
+                          ))
+                        ) : (
+                          <p>loading</p>
+                        )}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                  <Card border="dark">
+                    <Card.Header as="h3">Flag:</Card.Header>
+                    <Card.Img src={country?.flag}></Card.Img>
+                    {/* <Card.Body> 
                 <iframe
                   src={
                     "https://www.google.com/maps/embed/v1/place?key=" +
@@ -130,63 +151,58 @@ function Cuisine(id: any) {
                   loading="lazy"
                 ></iframe>
               </Card.Body> */}
+                  </Card>
+                </CardDeck>
+              </div>
+            </body>
           </Card>
-          </CardDeck>
-          
-        </div>
-      </body>
-      </Card>
-      {/* <div className="center">
-        <h4>Map Location</h4>
-        <iframe
-          src={cityMapURL}
-          width="600"
-          height="450"
-          loading="lazy"
-        ></iframe>
-      </div> */}
-                <iframe
-                  src={
-                    "https://www.google.com/maps/embed/v1/place?key=" +
-                    API_KEY +
-                    "&q=" +
-                    country?.name
-                  }
-                  width="100%"
-                  height="200%"
-                  loading="lazy"
-                ></iframe>
+          <h5>Restaurants with {cuisine?.name} food</h5>
+          {cuisine?.restaurants ? (
+            cuisine?.restaurants?.split(", ").map((r, index) => (
+              <a
+                href={
+                  "/restaurants/" + cuisine?.restaurant_ids.split(", ")[index]
+                }
+              >
+                {r}
+                <br />
+              </a>
+            ))
+          ) : (
+            <p>loading</p>
+          )}
 
-
-      <h5>Restaurants with {cuisine?.name} food</h5>
-      {cuisine?.restaurants ? (
-        cuisine?.restaurants?.split(", ").map((r, index) => (
-          <a
-            href={"/restaurants/" + cuisine?.restaurant_ids.split(", ")[index]}
-          >
-            {r}
-            <br />
-          </a>
-        ))
-      ) : (
-        <p>loading</p>
-      )}
-
-      <h5>Cities with {cuisine?.name} food</h5>
-      {cuisine?.cities ? (
-        cuisine?.cities?.split(", ").map((c, index) => (
-          <a href={"/cities/" + cuisine?.city_ids.split(", ")[index]}>
-            {c}
-            <br />
-          </a>
-        ))
-      ) : (
-        <p>loading</p>
-      )}
-      <p></p>
-      <p></p>
-      <p></p>
-      <p></p>
+          <h5>Cities with {cuisine?.name} food</h5>
+          {cuisine?.cities ? (
+            cuisine?.cities?.split(", ").map((c, index) => (
+              <a href={"/cities/" + cuisine?.city_ids.split(", ")[index]}>
+                {c}
+                <br />
+              </a>
+            ))
+          ) : (
+            <p>loading</p>
+          )}
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <div className="center">
+            <h4>Map Location</h4>
+            <div className="map-responsive">
+              <iframe
+                src={
+                  "https://www.google.com/maps/embed/v1/place?key=" +
+                  API_KEY +
+                  "&q=" +
+                  country?.name
+                }
+                width="600"
+                height="450"
+                loading="lazy"
+              ></iframe>
+            </div>
+          </div>
         </>
       ))}
     </div>
@@ -196,31 +212,31 @@ function Cuisine(id: any) {
     //     <h1 className="Instance-header">{cuisine ? cuisine.name : loading}</h1>
     //   </Row>
     //   {/*change orientation later */}
-    //   <Card> 
-        
+    //   <Card>
+
     //   </Card>
     //   <h5>{cuisine ? cuisine.description : loading}</h5>
     //   {countries?.map((country) => (
     //     <div>
     //       <h4>Country: {country?.name}</h4>
-          // <Row>
-          //   <Col xs={6} md={4}>
-          //     <Image src={country?.flag} fluid />
-          //   </Col>
-          //   <Col xs={6} md={4}>
-          //     <iframe
-          //       src={
-          //         "https://www.google.com/maps/embed/v1/place?key=" +
-          //         API_KEY +
-          //         "&q=" +
-          //         country?.name
-          //       }
-          //       width="450"
-          //       height="300"
-          //       loading="lazy"
-          //     ></iframe>
-          //   </Col>
-          // </Row>
+    // <Row>
+    //   <Col xs={6} md={4}>
+    //     <Image src={country?.flag} fluid />
+    //   </Col>
+    //   <Col xs={6} md={4}>
+    //     <iframe
+    //       src={
+    //         "https://www.google.com/maps/embed/v1/place?key=" +
+    //         API_KEY +
+    //         "&q=" +
+    //         country?.name
+    //       }
+    //       width="450"
+    //       height="300"
+    //       loading="lazy"
+    //     ></iframe>
+    //   </Col>
+    // </Row>
     //       <section>
     //         <h5>Alpha 3 Code:</h5>
     //         {country?.alpha3code}
@@ -240,88 +256,86 @@ function Cuisine(id: any) {
     //       {country?.capital}
 
     //       <h5>Bordering Countries</h5>
-          // {country?.borders.split(", ").map((border: any) => {
-          //   return <li>{border}</li>;
-          // })}
+    // {country?.borders.split(", ").map((border: any) => {
+    //   return <li>{border}</li>;
+    // })}
 
     //       <section>
     //         <h5>Time Zones</h5>
     //         {country?.timezones}
     //       </section>
     //       <h5>Translations</h5>
-          // {country ? (
-          //   Object.keys(country?.translations).map((key, index) => (
-          //     <p key={index}>
-          //       {" "}
-          //       {key} : {country?.translations[key]}
-          //     </p>
-          //   ))
-          // ) : (
-          //   <p>he</p>
-          // )}
+    // {country ? (
+    //   Object.keys(country?.translations).map((key, index) => (
+    //     <p key={index}>
+    //       {" "}
+    //       {key} : {country?.translations[key]}
+    //     </p>
+    //   ))
+    // ) : (
+    //   <p>he</p>
+    // )}
 
     //       <h5>Dishes</h5>
 
-          // {dishes ? (
-          //   dishes.map((cols: any) => (
-          //     <Row>
-          //       {cols.map((dish: any, i: any) => (
-          //         <Col className="col-sm-4 py-2">
-          //           <Figure>
-          //             <Figure.Image
-          //               width={170}
-          //               height={180}
-          //               alt="171x180"
-          //               src={dish.image_url}
-          //             />
-          //             <Figure.Caption>{dish.name}</Figure.Caption>
-          //           </Figure>
-          //         </Col>
-          //       ))}
-          //     </Row>
-          //   ))
-          // ) : (
-          //   <p>loading</p>
-          // )}
+    // {dishes ? (
+    //   dishes.map((cols: any) => (
+    //     <Row>
+    //       {cols.map((dish: any, i: any) => (
+    //         <Col className="col-sm-4 py-2">
+    //           <Figure>
+    //             <Figure.Image
+    //               width={170}
+    //               height={180}
+    //               alt="171x180"
+    //               src={dish.image_url}
+    //             />
+    //             <Figure.Caption>{dish.name}</Figure.Caption>
+    //           </Figure>
+    //         </Col>
+    //       ))}
+    //     </Row>
+    //   ))
+    // ) : (
+    //   <p>loading</p>
+    // )}
     //     </div>
     //   ))}
 
     //   {/*
     //   <p></p>
-  
 
-   
     // */}
 
-      // <h5>Restaurants with {cuisine?.name} food</h5>
-      // {cuisine?.restaurants ? (
-      //   cuisine?.restaurants?.split(", ").map((r, index) => (
-      //     <a
-      //       href={"/restaurants/" + cuisine?.restaurant_ids.split(", ")[index]}
-      //     >
-      //       {r}
-      //       <br />
-      //     </a>
-      //   ))
-      // ) : (
-      //   <p>loading</p>
-      // )}
+    // <h5>Restaurants with {cuisine?.name} food</h5>
+    // {cuisine?.restaurants ? (
+    //   cuisine?.restaurants?.split(", ").map((r, index) => (
+    //     <a
+    //       href={"/restaurants/" + cuisine?.restaurant_ids.split(", ")[index]}
+    //     >
+    //       {r}
+    //       <br />
+    //     </a>
+    //   ))
+    // ) : (
+    //   <p>loading</p>
+    // )}
 
-      // <h5>Cities with {cuisine?.name} food</h5>
-      // {cuisine?.cities ? (
-      //   cuisine?.cities?.split(", ").map((c, index) => (
-      //     <a href={"/cities/" + cuisine?.city_ids.split(", ")[index]}>
-      //       {c}
-      //       <br />
-      //     </a>
-      //   ))
-      // ) : (
-      //   <p>loading</p>
-      // )}
-      // <p></p>
-      // <p></p>
-      // <p></p>
-      // <p></p>
+    // <h5>Cities with {cuisine?.name} food</h5>
+    // {cuisine?.cities ? (
+    //   cuisine?.cities?.split(", ").map((c, index) => (
+    //     <a href={"/cities/" + cuisine?.city_ids.split(", ")[index]}>
+    //       {c}
+    //       <br />
+    //     </a>
+    //   ))
+    // ) : (
+    //   <p>loading</p>
+    // )}
+    // <p></p>
+    // <p></p>
+    // <p></p>
+    // <p></p>
     // </Container>
   );
 }
