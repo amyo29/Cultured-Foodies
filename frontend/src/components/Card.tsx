302709d4625df749d43941ef59da067c0caf61db
Vishal Tak <vishal.tak@shoplc.com>
2021-04-05 19:27:16 -0500

    Restaurants cards and maps are now responsive

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import "../styles/Footer.css";
 import Card from "react-bootstrap/Card";
 import "../styles/About.css";
-import Button from "react-bootstrap/Button";
-import Collapse from "react-bootstrap/Collapse";
-import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
@@ -12,7 +9,7 @@ const InfoCard = (props: any) => {
   const { title, description, img, link } = props;
   return (
     <div className="col-md-4">
-      <a href={link} className="card text-center" target="_blank">
+      <a href={link} className="card text-center">
         <img src={img} className="card-img-top info-card-img-top" />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
@@ -26,7 +23,7 @@ const InfoCard = (props: any) => {
 const ProfileCard = (props: any) => {
   const { name, img, role, bio, commits, issues, tests, linkedin } = props;
   return (
-    <a href={linkedin} className="card text-center" target="_blank">
+    <a href={linkedin} className="card text-center">
       <img src={img} className="card-img-top profile-img img-fluid" />
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
