154cbd1ea7e3d43c69072dbeece8a6e20da1b2a3
Vishal Tak <vishal.tak@shoplc.com>
2021-04-05 21:51:50 -0500

    Cities instance pages looking clean

--- frontend/src/pages/Cities/City.tsx
+++ frontend/src/pages/Cities/City.tsx
@@ -53,154 +53,162 @@ function City(id: any) {
 
   return (
     <div>
-      <Image src={city?.imagesweb} fluid />
-      <Container>
-        <h1>{city?.name}</h1>
+      <img src={city?.imagesweb} className = "city-img-top"/>
+      <h1 className="text-center">{city?.name}</h1>
+      <div className="row m-0">
+        <div className="col margin">
+          <div className="card">
+            <div className="card-body align-self-start">
+              <h2>{city?.full_name}</h2>
 
-        <section>
-          <h2>About the City</h2>
-          {city?.full_name}
+              <h5>Summary</h5>
+              {city?.summary.replace(/(<([^>]+)>)/gi, "")}
 
-          <h5>Summary</h5>
-          {city?.summary.replace(/(<([^>]+)>)/gi, "")}
+              <h5>State</h5>
+              {city?.state}
 
-          <h5>State</h5>
-          {city?.state}
+              <h5>Population</h5>
+              {city?.population.toLocaleString()}
 
-          <h5>Population</h5>
-          {city?.population.toLocaleString()}
+              <h5>Location</h5>
+              <h6>Latitude</h6>
+              {city?.latitude}
+              <h6>Longitude</h6>
+              {city?.longitude}
 
-          <h5>Location</h5>
-          <h6>Latitude</h6>
-          {city?.latitude}
-          <h6>Longitude</h6>
-          {city?.longitude}
-
-          <h5>Timezone</h5>
-          {city?.timezone}
-        </section>
-
-        <section style={{ alignItems: "left", paddingLeft: "1px" }}>
-          <h5>Scores</h5>
-          <h6>Business Freedom</h6>
-          {city?.business_freedom ? parseFloat(city?.business_freedom.toFixed(4)) : " "}  
-          {/* {city?.business_freedom && <div>here</div>} */}
-
-          <h6>Cost of Living</h6>
-          {city?.cost_of_living ? parseFloat(city?.cost_of_living.toFixed(4)) : " "}   
-
-          <h6>Economy</h6>
-          {city?.economy ? parseFloat(city?.economy.toFixed(4)) : " "}
-
-          <h6>Housing</h6>
-          {city?.housing ? parseFloat(city?.housing.toFixed(4)) : " "}
-
-          <h6>Commute</h6>
-          {city?.commute ? parseFloat(city?.commute.toFixed(4)) : " "}
-
-          <h6>Education</h6>
-          {city?.education ? parseFloat(city?.education.toFixed(4)) : " "}
-
-          <h6>Environmental Quality</h6>
-          {city?.environmental_quality ? parseFloat(city?.environmental_quality.toFixed(4)) : " "}
-
-          <h6>Healthcare</h6>
-          {city?.healthcare ? parseFloat(city?.healthcare.toFixed(4)) : " "}  
-
-          <h6>Leisure and Culture</h6>
-          {city?.leisure_culture ? parseFloat(city?.leisure_culture.toFixed(4)) : " "}
-
-          <h6>Outdoors</h6>
-          {city?.outdoors ? parseFloat(city?.outdoors.toFixed(4)) : " "}
-
-          <h6>Safety</h6>
-          {city?.safety ? parseFloat(city?.safety.toFixed(4)) : " "}
-
-          <h6>Taxation</h6>
-          {city?.taxation ? parseFloat(city?.taxation.toFixed(4)) : " "}
-
-          <h6>Travel and Connectivity</h6>
-          {city?.travel_connectivity ? parseFloat(city?.travel_connectivity.toFixed(4)) : " "}
-
-          <h6>Internet Access</h6>
-          {city?.internet_access ? parseFloat(city?.internet_access.toFixed(4)) : " "}
-
-          <h6>Venture Capital</h6>
-          {city?.venture_capital ? parseFloat(city?.venture_capital.toFixed(4)) : " "}   
-        </section>
-        <section>
-          <h5>Restaurants in {city?.name}</h5>
-          {city?.restaurants ? (
-            city?.restaurants?.split(", ").map((r, index) => (
-              <a
-                href={"/restaurants/" + city?.restaurant_ids.split(", ")[index]}
-              >
-                {r}
-                <br />
-              </a>
-            ))
-          ) : (
-            <p>loading</p>
-          )}
-
-          <h5>Cuisines of {city?.name}</h5>
-          {cuisines?.map((c) => (
-            <a href={"/cuisines/" + c.id}>
-              {c.name}
-              <br />{" "}
-              <img src={c.dishes[0].image_url} width="200" height="200"></img>
-            </a>
-          ))}
-        </section>
-
-        {/* <h5>Health</h5>
-        {data["recipe"]["healthLabels"].map((healthLabel: any) => {
-          return <li>{healthLabel}</li>;
-        })}
-      </section>
-
-      <Image src={data["recipe"]["image"]} fluid />
-
-      <p>{data["recipe"]["source"]}</p>
-
-      <h5>Calories: {data["recipe"]["calories"]}</h5>
-      <h5>Total energy (kcal): {data["recipe"]["totalNutrients"]["ENERC_KCAL"]["quantity"]}</h5>
-
-      <p>
-        <h5>Ingredients</h5>
-        {data["recipe"]["ingredientLines"].map((ingredient: any) => {
-          return <li>{ingredient}</li>;
-        })}
-      </p>
-
-      <h5>Restrictions</h5>
-      {data["recipe"]["cautions"].length == 0
-        ? "No restrictions"
-        : data["recipe"]["cautions"].map((caution: any) => {
-            return <li>{caution}</li>;
-          })
-      }
-
-      <h5>Country: </h5>
-      <a href={"/countries/" + countryIndex}>{country["name"]}</a>
-
-      <h5>Mentioned in news articles: </h5>
-      <a href={"/news/" + newsIndex}>{article["title"]}</a>
-      <br />
-
+              <h5>Timezone</h5>
+              {city?.timezone}
+            </div>
+          </div>
+        </div>
+        <div className="col margin">
+          <div className="card">
+          <table className="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Attribute</th>
+                <th scope="col">Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td scope="row">Business Freedom</td>
+                <td>
+                  {city?.business_freedom
+                    ? parseFloat(city?.business_freedom.toFixed(4))
+                    : " "}
+                </td>
+              </tr>
+              <tr>
+                <td scope="row">Cost of Living</td>
+                <td>
+                  {city?.cost_of_living
+                    ? parseFloat(city?.cost_of_living.toFixed(4))
+                    : " "}
+                </td>
+              </tr>
+              <tr>
+                <td scope="row">Economy</td>
+                <td>
+                  {city?.economy ? parseFloat(city?.economy.toFixed(4)) : " "}
+                </td>
+              </tr>
+              <tr>
+                <td scope="row">Housing</td>
+                <td>
+                  {city?.housing ? parseFloat(city?.housing.toFixed(4)) : " "}
+                </td>
+              </tr>
+              <tr>
+                <td scope="row">Commute</td>
+                <td>
+                  {city?.commute ? parseFloat(city?.commute.toFixed(4)) : " "}
+                </td>
+              </tr>
+              <tr>
+                <td scope="row">Education</td>
+                <td>
+                  {city?.education
+                    ? parseFloat(city?.education.toFixed(4))
+                    : " "}
+                </td>
+              </tr>
+              <tr>
+                <td scope="row">Environmental Quality</td>
+                <td>
+                  {city?.environmental_quality
+                    ? parseFloat(city?.environmental_quality.toFixed(4))
+                    : " "}
+                </td>
+              </tr>
+              <tr>
+                <td scope="row">Healthcare</td>
+                <td>
+                  {city?.healthcare
+                    ? parseFloat(city?.healthcare.toFixed(4))
+                    : " "}{" "}
+                </td>
+              </tr>
+              <tr>
+                <td scope="row">Leisure and Culture</td>
+                <td>
+                  {city?.leisure_culture
+                    ? parseFloat(city?.leisure_culture.toFixed(4))
+                    : " "}
+                </td>
+              </tr>
+              <tr>
+                <td scope="row">Outdoors</td>
+                <td>
+                  {city?.outdoors ? parseFloat(city?.outdoors.toFixed(4)) : " "}{" "}
+                </td>
+              </tr>
+              <tr>
+                <td scope="row">Safety</td>
+                <td>
+                  {city?.safety ? parseFloat(city?.safety.toFixed(4)) : " "}
+                </td>
+              </tr>
+              <tr>
+                <td scope="row">Taxation</td>
+                <td>
+                  {city?.taxation ? parseFloat(city?.taxation.toFixed(4)) : " "}
+                </td>
+              </tr>
+              <tr>
+                <td scope="row">Travel and Connectivity</td>
+                <td>
+                  {city?.travel_connectivity
+                    ? parseFloat(city?.travel_connectivity.toFixed(4))
+                    : " "}
+                </td>
+              </tr>
+              <tr>
+                <td scope="row">Internet Access</td>
+                <td>
+                  {" "}
+                  {city?.internet_access
+                    ? parseFloat(city?.internet_access.toFixed(4))
+                    : " "}
+                </td>
+              </tr>
+              <tr>
+                <td scope="row">Venture Capital</td>
+                <td>
+                  {" "}
+                  {city?.venture_capital
+                    ? parseFloat(city?.venture_capital.toFixed(4))
+                    : " "}
+                </td>
+              </tr>
+            </tbody>
+            <caption>City Urban Area Quality of Living Scores</caption>
+          </table>
+          </div>
+        </div>
+      </div>
       <div className="center">
-        <h3>Recipe video</h3>
-        <iframe
-          src={youTubeURL}
-          width="50%"
-          height="400"
-          allow="fullscreen"
-          title="video"
-        />
-      </div> */}
-      </Container>
-      <div className="center">
-        <h4>Map Location</h4>
+        <h2>Map Location</h2>
         <div className="map-responsive">
           <iframe
             src={cityMapURL}
@@ -208,8 +216,8 @@ function City(id: any) {
             height="450"
             loading="lazy"
           ></iframe>
-          </div>
         </div>
+      </div>
     </div>
   );
 }
