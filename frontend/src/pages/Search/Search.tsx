673b67cfe4268460c6b2c7b58be224562bb5c37c
lucinda <lucinda.onguyen@gmail.com>
2021-04-08 17:40:49 -0500

    search file

--- frontend/src/pages/Search/Search.tsx
+++ frontend/src/pages/Search/Search.tsx
@@ -10,20 +10,16 @@ import {
 function Search(query: any) {
   console.log("searching", query);
   const searchClient = algoliasearch(
-    "latency",
-    "6be0576ff61c053d5f9a3225e2a90f76"
+    "2EZLZI9A68",
+    "d04bf0d390cc40ca14eebcd6d0acea55"
   );
 
   return (
     <div>
       <h1>Search Results for {query.q}</h1>
-      {/* <InstantSearch
-    appId="2EZLZI9A68"
-    apiKey="d04bf0d390cc40ca14eebcd6d0acea55"
-    indexName="bestbuy"
-  > */}
 
-      <InstantSearch searchClient={searchClient} indexName="instant_search">
+
+      <InstantSearch searchClient={searchClient} indexName="cities">
         <header>
           <SearchBox translations={{ placeholder: "Search Box" }} />
           {/* <Hits hitComponent={Hit} /> */}
