20a6f2e8ccc22b2c66c2929966a8c9fe77eb176b
lucinda <lucinda.onguyen@gmail.com>
2021-04-10 16:53:10 -0500

    highlight on sitewide

--- frontend/src/pages/Search/Search.tsx
+++ frontend/src/pages/Search/Search.tsx
@@ -5,10 +5,27 @@ import {
   SearchBox,
   Hits,
   Highlight,
+  Index,
 } from "react-instantsearch-dom";
+function CityHit(props: any) {
+  return (
+    <div>
+      <div className="name">
+        <Highlight tagName="mark" attribute="name" hit={props.hit} />
+      </div>
+      <div className="hit-description">
+        <Highlight tagName="mark" attribute="summary" hit={props.hit} />
+      </div>
+      <div className="hit-description">
+        <Highlight tagName="mark" attribute="state" hit={props.hit} />
+      </div>
+    </div>
+  );
+}
 
 function Search(query: any) {
-  console.log("searching", query);
+  console.log("here");
+  console.log("searching", query["q"]);
   const searchClient = algoliasearch(
     "2EZLZI9A68",
     "d04bf0d390cc40ca14eebcd6d0acea55"
@@ -16,14 +33,21 @@ function Search(query: any) {
 
   return (
     <div>
-      <h1>Search Results for {query.q}</h1>
+      <h1>Search Results for {query["q"]}</h1>
 
+      <InstantSearch
+        indexName="cities"
+        searchClient={searchClient}
+        searchState={{ query: query["q"] }}
+      >
+        <div style={{ display: "none" }}>
+          <SearchBox />
+        </div>
 
-      <InstantSearch searchClient={searchClient} indexName="cities">
-        <header>
-          <SearchBox translations={{ placeholder: "Search Box" }} />
-          {/* <Hits hitComponent={Hit} /> */}
-        </header>
+        <Index indexName="cities">
+          <h2>index: Cities</h2>
+          <Hits hitComponent={CityHit} />
+        </Index>
       </InstantSearch>
     </div>
   );
