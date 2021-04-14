061e619c71e348ca2b7051ca8039cdd27179e36e
Vishal Tak <vishal.tak@shoplc.com>
2021-04-13 21:00:33 -0500

    Fixed css error in cusines and search

--- frontend/src/pages/Search/Search.tsx
+++ frontend/src/pages/Search/Search.tsx
@@ -14,7 +14,7 @@ import {
 import { DialpadSharp } from "@material-ui/icons";
 function CuisineHit(props: any) {
   return (
-    <div className="card" id="module" style={{ width: "40rem" }}>
+    <div className="card" id="search-page" style={{ width: "40rem" }}>
       <div className="card-body align-self-start">
         <p className="collapse" id="collapseParagraph" aria-expanded="false">
           <div className="name">
