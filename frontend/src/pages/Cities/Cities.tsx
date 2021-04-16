de55bd1f6d9a7c95683843d58e2e887ca3cd71ec
amyo29 <yawen1999@gmail.com>
2021-04-16 00:28:56 -0500

    city model page ui

--- frontend/src/pages/Cities/Cities.tsx
+++ frontend/src/pages/Cities/Cities.tsx
@@ -322,7 +322,7 @@ function Cities() {
                 </Row>
                 <Row style={rowStyle} className="mt-5">
                   <Card.Subtitle style={subtitleTextStyle}>
-                    <h4> Wherever you are, wherever you're headed, all you want to know about your city is just a click away!</h4>
+                    <h4> 🌆 Wherever you are, wherever you're headed, all you want to know about your city is just a click away! 🏙️ </h4>
                   </Card.Subtitle>
                 </Row>
                 <Row style={rowStyle}>
