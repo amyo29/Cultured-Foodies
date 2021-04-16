70b986c0880488b2da1ab287ee22cca2303ff891
Tim Nguyen <nguye@MacBook-Pro-2.local>
2021-04-16 17:22:00 -0500

    Added highlighting to Cities and Cuisines

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -11,6 +11,7 @@ import {
   Highlight,
   Index,
 } from "react-instantsearch-dom";
+import Highlighter from "react-highlight-words";
 
 const InfoCard = (props: any) => {
   const { title, description, img, link } = props;
@@ -46,27 +47,79 @@ const ProfileCard = (props: any) => {
 
 const CitiesCard = (props: any) => {
   const { city } = props;
-  let leisureCulture = (parseInt(city["leisure_culture"]!) == 0) ? "No Data" : parseFloat(city["leisure_culture"].toFixed(2));
-  let costOfLiving = (parseInt(city["cost_of_living"]!) == 0) ? "No Data" : parseFloat(city["cost_of_living"].toFixed(2));
-  let environmentalQuality = (parseInt(city["environmental_quality"]!) == 0) ? "No Data" : parseFloat(city["environmental_quality"].toFixed(2));
-  let travelConnectivity = (parseInt(city["travel_connectivity"]!) == 0) ? "No Data" : parseFloat(city["travel_connectivity"].toFixed(2));
+  let leisureCulture =
+    parseInt(city["leisure_culture"]!) == 0
+      ? "No Data"
+      : parseFloat(city["leisure_culture"].toFixed(2));
+  let costOfLiving =
+    parseInt(city["cost_of_living"]!) == 0
+      ? "No Data"
+      : parseFloat(city["cost_of_living"].toFixed(2));
+  let environmentalQuality =
+    parseInt(city["environmental_quality"]!) == 0
+      ? "No Data"
+      : parseFloat(city["environmental_quality"].toFixed(2));
+  let travelConnectivity =
+    parseInt(city["travel_connectivity"]!) == 0
+      ? "No Data"
+      : parseFloat(city["travel_connectivity"].toFixed(2));
+
+  let searchWords = props.searchQuery.split(" ");
 
   return (
     <a href={"/cities/" + city["id"]} className="card">
-      <h5 className="card-title mt-3">{city["name"]}</h5>
+      <h5 className="card-title mt-3">
+        <Highlighter
+          highlightClassName="highlight-class"
+          searchWords={searchWords}
+          textToHighlight={city["name"]}
+        ></Highlighter>
+      </h5>
       <img src={city["imagesmobile"]} className="city-card-img-top" />
       <div className="card-body text-left">
         <p>
-          <b>State: </b> {city["state"]} <br />
-          <b>Leisure and Culture: </b>{" "}
-          {leisureCulture} <br />
-          <b>Cost of Living: </b>{" "}
-          {costOfLiving} <br />
-          <b>Environmental Quality: </b>{" "}
-          {environmentalQuality} <br />
-          <b>Travel Connectivity: </b>{" "}
-          {travelConnectivity} <br />
-          <b>Population: </b> {city["population"].toLocaleString()} <br />
+          <b>State: </b>
+          <Highlighter
+            highlightClassName="highlight-class"
+            searchWords={searchWords}
+            textToHighlight={city["state"]}
+          ></Highlighter>
+          <br />
+          <b>Leisure and Culture: </b>
+          <Highlighter
+            highlightClassName="highlight-class"
+            searchWords={searchWords}
+            textToHighlight={String(leisureCulture)}
+          ></Highlighter>
+          <br />
+          <b>Cost of Living: </b>
+          <Highlighter
+            highlightClassName="highlight-class"
+            searchWords={searchWords}
+            textToHighlight={String(costOfLiving)}
+          ></Highlighter>
+          <br />
+          <b>Environmental Quality: </b>
+          <Highlighter
+            highlightClassName="highlight-class"
+            searchWords={searchWords}
+            textToHighlight={String(environmentalQuality)}
+          ></Highlighter>
+          <br />
+          <b>Travel Connectivity: </b>
+          <Highlighter
+            highlightClassName="highlight-class"
+            searchWords={searchWords}
+            textToHighlight={String(travelConnectivity)}
+          ></Highlighter>
+          <br />
+          <b>Population: </b>
+          <Highlighter
+            highlightClassName="highlight-class"
+            searchWords={searchWords}
+            textToHighlight={city["population"].toLocaleString()}
+          ></Highlighter>
+          <br />
         </p>
       </div>
     </a>
@@ -74,9 +127,16 @@ const CitiesCard = (props: any) => {
 };
 const CuisinesCard = (props: any) => {
   const { cuisine, countries, loaded } = props;
+  let searchWords = props.searchQuery.split(" ");
   return (
     <a href={"/cuisines/" + cuisine["id"]} id="module" className="card">
-      <h5 className="card-title mt-3">{cuisine["name"]}</h5>
+      <h5 className="card-title mt-3">
+        <Highlighter
+          highlightClassName="highlight-class"
+          searchWords={searchWords}
+          textToHighlight={cuisine["name"]}
+        ></Highlighter>
+      </h5>
       <img src={cuisine.dishes[0].image_url} className="city-card-img-top" />
       <div className="card-body align-self-start">
         <p>
@@ -92,38 +152,102 @@ const CuisinesCard = (props: any) => {
                         aria-expanded="false"
                       >
                         <b>Country: </b>
-                        {cuisine["country"].split(", ")[i]} <br />
+                        <Highlighter
+                          highlightClassName="highlight-class"
+                          searchWords={searchWords}
+                          textToHighlight={cuisine["country"].split(", ")[i]}
+                        ></Highlighter>
+                        <br />
                         <b>Capital: </b>
-                        {countries[parseInt(cID)].capital} <br />
+                        <Highlighter
+                          highlightClassName="highlight-class"
+                          searchWords={searchWords}
+                          textToHighlight={countries[parseInt(cID)].capital}
+                        ></Highlighter>
+                        <br />
                         <b>Region: </b>
-                        {countries[parseInt(cID)].region} <br />
+                        <Highlighter
+                          highlightClassName="highlight-class"
+                          searchWords={searchWords}
+                          textToHighlight={countries[parseInt(cID)].region}
+                        ></Highlighter>
+                        <br />
                         <b>Subregion: </b>
-                        {countries[parseInt(cID)].subregion} <br />
+                        <Highlighter
+                          highlightClassName="highlight-class"
+                          searchWords={searchWords}
+                          textToHighlight={countries[parseInt(cID)].subregion}
+                        ></Highlighter>
+                        <br />
                         <b>Population: </b>
-                        {countries[parseInt(cID)].population.toLocaleString()}
+                        <Highlighter
+                          highlightClassName="highlight-class"
+                          searchWords={searchWords}
+                          textToHighlight={countries[
+                            parseInt(cID)
+                          ].population.toLocaleString()}
+                        ></Highlighter>
                         <br />
                         <b>Area: </b>
-                        {countries[parseInt(cID)].area.toLocaleString()} km
-                        <sup>2</sup>
+                        <Highlighter
+                          highlightClassName="highlight-class"
+                          searchWords={searchWords}
+                          textToHighlight={countries[
+                            parseInt(cID)
+                          ].area.toLocaleString()}
+                        ></Highlighter>
+                        &nbsp;km<sup>2</sup>
                       </p>
                     </>
                   ) : (
                     <>
                       <p>
                         <b>Country: </b>
-                        {cuisine["country"].split(", ")[i]} <br />
+                        <Highlighter
+                          highlightClassName="highlight-class"
+                          searchWords={searchWords}
+                          textToHighlight={cuisine["country"].split(", ")[i]}
+                        ></Highlighter>
+                        <br />
                         <b>Capital: </b>
-                        {countries[parseInt(cID)].capital} <br />
+                        <Highlighter
+                          highlightClassName="highlight-class"
+                          searchWords={searchWords}
+                          textToHighlight={countries[parseInt(cID)].capital}
+                        ></Highlighter>
+                        <br />
                         <b>Region: </b>
-                        {countries[parseInt(cID)].region} <br />
+                        <Highlighter
+                          highlightClassName="highlight-class"
+                          searchWords={searchWords}
+                          textToHighlight={countries[parseInt(cID)].region}
+                        ></Highlighter>
+                        <br />
                         <b>Subregion: </b>
-                        {countries[parseInt(cID)].subregion} <br />
+                        <Highlighter
+                          highlightClassName="highlight-class"
+                          searchWords={searchWords}
+                          textToHighlight={countries[parseInt(cID)].subregion}
+                        ></Highlighter>
+                        <br />
                         <b>Population: </b>
-                        {countries[parseInt(cID)].population.toLocaleString()}
+                        <Highlighter
+                          highlightClassName="highlight-class"
+                          searchWords={searchWords}
+                          textToHighlight={countries[
+                            parseInt(cID)
+                          ].population.toLocaleString()}
+                        ></Highlighter>
                         <br />
                         <b>Area: </b>
-                        {countries[parseInt(cID)].area.toLocaleString()} km
-                        <sup>2</sup>
+                        <Highlighter
+                          highlightClassName="highlight-class"
+                          searchWords={searchWords}
+                          textToHighlight={countries[
+                            parseInt(cID)
+                          ].area.toLocaleString()}
+                        ></Highlighter>
+                        &nbsp;km<sup>2</sup>
                       </p>
                     </>
                   )}
@@ -151,23 +275,21 @@ const CuisinesCard = (props: any) => {
   );
 };
 
-
 const SearchCard = (props: any) => {
   const { result } = props;
   return (
     <div>
-    <div className="name">
-      <Highlight tagName="mark" attribute="name" hit={props.hit} />
+      <div className="name">
+        <Highlight tagName="mark" attribute="name" hit={props.hit} />
+      </div>
+      <div className="hit-description">
+        <Highlight tagName="mark" attribute="summary" hit={props.hit} />
+      </div>
+      <div className="hit-description">
+        <Highlight tagName="mark" attribute="state" hit={props.hit} />
+      </div>
     </div>
-    <div className="hit-description">
-      <Highlight tagName="mark" attribute="summary" hit={props.hit} />
-    </div>
-    <div className="hit-description">
-      <Highlight tagName="mark" attribute="state" hit={props.hit} />
-    </div>
-  </div>
   );
 };
 
-
-export { InfoCard, ProfileCard, CitiesCard, CuisinesCard, SearchCard};
+export { InfoCard, ProfileCard, CitiesCard, CuisinesCard, SearchCard };
