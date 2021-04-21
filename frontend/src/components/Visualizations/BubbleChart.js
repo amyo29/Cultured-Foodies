aa16430e3d29f7338cdd4fd679115047e887b7f2
amyo29 <yawen1999@gmail.com>
2021-04-21 18:14:46 -0500

    add bubble chart visualization and scrape agency provider data

--- frontend/src/components/Visualizations/BubbleChart.js
+++ frontend/src/components/Visualizations/BubbleChart.js
@@ -1,24 +1,9 @@
-import React from "react";
-import fakeData from "../vizualization_data/very_fake_data.json";
-import BarChart from "../components/Visualizations/BarChart";
-import NavBarSolid from "../components/NavBarSolid";
-// import BubbleChartFunc from "../components/Visualizations/BubbleChart";
+import React from 'react';
 import BubbleChart from "@weknow/react-bubble-chart-d3";
 
-function Visualizations() {
+function BubbleChartFunc () {
   return (
-    <div>
-       <NavBarSolid />
-      Visualizations
-      <BarChart
-        data={fakeData}
-        xAttr="country"
-        yAttr="emissions"
-        xLabel="Countries"
-        yLabel="Carbon Emissions (ppm)"
-      />
-      {/* <BubbleChartFunc/> */}
-      <BubbleChart
+    <BubbleChart
       width={1000}
       height={800}
       legendFont={{
@@ -54,8 +39,7 @@ function Visualizations() {
         { label: "Content", value: 26 },
       ]}
     />
-    </div>
   );
 }
 
-export default Visualizations;
+export default BubbleChartFunc;
