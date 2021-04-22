062108c5acdef855c2151a526342e08537a2e5fc
Gear-500 <joshuanms@gmail.com>
2021-04-22 18:29:16 -0500

    Draft 1- edited visualizations.jsx styles

--- frontend/src/pages/Visualizations.jsx
+++ frontend/src/pages/Visualizations.jsx
@@ -1,22 +1,67 @@
-import React from "react";
-import fakeData from "../visualization_data/very_fake_data.json";
+
+import React, { useEffect, useState } from "react";
+import {Row, Container} from "react-bootstrap"
+import expeditionsPerCountry from "../visualizationData/providerData/expeditionsPerCountry.json";
 import BarChart from "../components/Visualizations/BarChart";
 import NavBarSolid from "../components/NavBarSolid";
-import BubbleChart from "../components/Visualizations/BubbleChart";
+import "../styles/Visualizations.css";
+
+//import CSS from "csstype";
+//import BubbleChart from "../components/Visualizations/BubbleChart";
+
+// const rowStyle: CSS.Properties = {
+//   textAlign: "center",
+//   alignItems: "center",
+//   justifyContent: "center",
+// };
 
 function Visualizations() {
+  const [key, setKey] = useState('home');
   return (
     <div>
       <NavBarSolid />
-      Visualizations
-      <BarChart
-        data={fakeData}
-        xAttr="country"
-        yAttr="emissions"
-        xLabel="Countries"
-        yLabel="Carbon Emissions (ppm)"
-      />
-    <BubbleChart/>
+      <Container fluid>
+      <Row class="center">
+        <h1 class="custom1">Visualizations</h1>
+      </Row>
+      <Row class="center">
+        <h2 class="custom1">Our Data</h2>
+      </Row>
+      <Row class="center">
+        <h2 class="custom1">Provider Data</h2>
+      </Row>
+      <Row class="center">
+        <h3 class="custom1">Expeditions per country</h3> 
+      </Row>
+      <Row class="center">
+          <BarChart
+            data={expeditionsPerCountry}
+            xAttr="country"
+            yAttr="expeditions"
+            xLabel="Countries"
+            yLabel="Expeditions"
+          />
+      </Row>
+      <Row class="center">
+          <BarChart
+            data={expeditionsPerCountry}
+            xAttr="country"
+            yAttr="expeditions"
+            xLabel="Countries"
+            yLabel="Expeditions"
+          />
+      </Row>
+      <Row class="center">
+          <BarChart
+            data={expeditionsPerCountry}
+            xAttr="country"
+            yAttr="expeditions"
+            xLabel="Countries"
+            yLabel="Expeditions"
+          />
+      </Row>
+    
+      </Container>
     </div>
   );
 }
