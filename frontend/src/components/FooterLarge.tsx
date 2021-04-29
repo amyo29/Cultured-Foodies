ac20617936945413105f509cea2c1dd68859e428
Vishal Tak <vishal.tak@shoplc.com>
2021-04-29 09:36:41 -0500

    fixed copyright text spacing at the bottom of the footer

--- frontend/src/components/FooterLarge.tsx
+++ frontend/src/components/FooterLarge.tsx
@@ -9,6 +9,7 @@ function FooterLarge() {
 				<div className='footerDataLong'>
 					<h1 className='header1'>Cultured Foodies</h1>
 						Explore the intersection of cuisine and culture.
+						<br/>
 				</div>
 				
 				{/* Short column with other links */}
@@ -18,19 +19,21 @@ function FooterLarge() {
 					<a href="/cuisines">Cuisines</a>
 					<a href="/cities">Cities</a>
 					<a href="/restaurants">Restaurants</a>
+					<br/>
 				</div>
 				<div className='footerDataShort'>
 					<h1 className='header2'>Other Links</h1>
 					<a href="/about">About</a>
 					<a href="/visualizations">Visualizations</a>
 					<a href="/provider-visualizations">Provider's Visualizations</a>
-
+					<br/>
 				</div>
 				
 			</div>
 			{/* Bottom of footer: website copyright */}
-			
+
 			<div className='copyrightText' style={{fontSize: "large"}}>
+			
 				Copyright © 2021 Cultured Foodies
 			</div>   
 		</footer>
